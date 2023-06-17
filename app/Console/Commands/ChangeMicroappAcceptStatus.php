7fbd1ff73012592f12a30b3ba8be6438c1757b11
ktsouvalis <67092537+ktsouvalis@users.noreply.github.com>
2023-06-17 19:09:31 +0300

    implement the Command that sets the 'accepts' field of the microapps according to opens_at and closes_at. add the line to the Kernel schedule

---
+++ app/Console/Commands/ChangeMicroappAcceptStatus.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Microapp;
+use Illuminate\Console\Command;
+
+class ChangeMicroappAcceptStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'microapps:accept_not';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Sets the accepts field of a microapp 0 or 1 according to opens_at and closes_at of the microapps. Scheduled to run daily";
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $microapps_to_close = Microapp::whereDate('closes_at', '<', now())->get();
+
+        foreach($microapps_to_close as $microapp) {
+            $microapp->update(['accepts' => 0]);
+        }
+
+        $microapps_to_open = Microapp::whereDate('opens_at', '=', now())->get();
+
+        foreach($microapps_to_open as $microapp) {
+            $microapp->update(['accepts' => 1]);
+        }
+
+
+        $this->info('Microapps updated successfully.');
+        
+    }
+}

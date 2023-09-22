40cfad2726a92706c649eb4b17d970eace92d18e
Konstantinos Tsouvalis <ktsouvalis@sch.gr>
2023-09-22 12:28:04 +0300

    fix logs to show ->getMessage()

--- app/Http/Controllers/microapps/TicketsController.php
+++ app/Http/Controllers/microapps/TicketsController.php
@@ -24,19 +24,18 @@ class TicketsController extends Controller
                 'subject' => $request->input('subject'),
                 'comments' => Auth::guard('school')->user()->name.": ".$request->input('comments'),
                 'solved' => 0
-            ]);
-
-            
+            ]); 
         }
         catch(Throwable $e){
             Log::channel('throwable_db')->error(Auth::guard('school')->user()->name." ticket creation failed");
             return redirect(url('/school_app/tickets'))->with('failure','Κάποιο σφάλμα προέκυψε, προσπαθήστε ξανά');
         }
 
-        Log::channel('stakeholders_microapps')->info(Auth::guard('school')->user()->name." ticket $new_ticket->id creation success");
-        Log::channel('tickets')->info(Auth::guard('school')->user()->name." ticket $new_ticket->id creation success");
+        Log::channel('stakeholders_microapps')->info($new_ticket->school->name." ticket $new_ticket->id creation success");
+        Log::channel('tickets')->info($new_ticket->school->name." ticket $new_ticket->id creation success");
 
         Mail::to("plinet_pe@dipe.ach.sch.gr")->send(new TicketCreated($new_ticket));
+        Mail::to($new_ticket->school->mail)->send(new TicketCreated($new_ticket));
         foreach(Superadmin::all() as $superadmin){
             Mail::to($superadmin->user->email)->send(new TicketCreated($new_ticket));    
         }
@@ -48,13 +47,9 @@ class TicketsController extends Controller
     public function update_ticket(Ticket $ticket, Request $request){
         if(Auth::user()){
             $name = Auth::user()->username;
-            $mail_to = $ticket->school->mail;
-            $link_to_send = $ticket->school->md5;
         }
         else if(Auth::guard('school')->user()){
             $name = Auth::guard('school')->user()->name;
-            $mail_to = "plinet_pe@dipe.ach.sch.gr";
-            $link_to_send=null;
         }
         $new_string = "\n".$name.": ".$request->input('comments');
         $updated_comments = $ticket->comments.$new_string;
@@ -62,7 +57,11 @@ class TicketsController extends Controller
         $ticket->solved=0;
         $ticket->save();
         
-        Mail::to($mail_to)->send(new TicketUpdated($ticket, $new_string, $name, $link_to_send));
+        Mail::to($ticket->school->mail)->send(new TicketUpdated($ticket, $new_string, $ticket->school->name, $ticket->school->md5));
+        Mail::to("plinet_pe@dipe.ach.sch.gr")->send(new TicketUpdated($ticket, $new_string, "Γραφείο Πλη.Νε.Τ.", ""));
+        foreach(Superadmin::all() as $superadmin){
+            Mail::to($superadmin->user->email)->send(new TicketUpdated($ticket, $new_string, $superadmin->user->username, ""));    
+        }
         Log::channel('tickets')->info($name." ticket $ticket->id updated comments");
         return back()->with('success', 'Το δελτίο ανανεώθηκε με την απάντησή σας');
     }

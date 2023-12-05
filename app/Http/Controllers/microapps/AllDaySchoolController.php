ba48f5f5424b5a3f0dc1b1ea1fd29acd35a11517
Konstantinos Tsouvalis <ktsouvalis@sch.gr>
2023-12-05 09:20:15 +0200

    fix all_day counting bug

--- app/Http/Controllers/microapps/AllDaySchoolController.php
+++ app/Http/Controllers/microapps/AllDaySchoolController.php
@@ -69,10 +69,10 @@ class AllDaySchoolController extends Controller
                     if($time=='15:00' or $time=='3:00:00 μμ' or $time==0.625 or $time=='15:00 ή 14:50' or $time=='15:00 ή 14:55' or $time=='15:00:00'){
                         $nos3++;
                     }
-                    else if($time=='16:00' or $time=='4:00:00 μμ' or abs($time-0.66666666666667< 0.00001) or $time=='16:00 ή 15:50' or $time=='16:00:00') {
+                    else if($time=='16:00' or $time=='4:00:00 μμ' or abs(strtotime($time)/86400-0.66666666666667)< 0.00001 or $time=='16:00 ή 15:50' or $time=='16:00:00') {
                         $nos4++;
                     }
-                    else if($time=='17:30' or $time=='5:30:00 μμ' or abs($time-0.72916666666667< 0.00001) or $time=='17:30:00'){
+                    else if($time=='17:30' or $time=='5:30:00 μμ' or abs(strtotime($time)/86400-0.72916666666667)< 0.00001 or $time=='17:30:00'){
                         $nos5++;
                     }
 

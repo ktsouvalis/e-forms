c9e32fe3a001fc5f38b0de4df80f0af9388be012
Konstantinos Stefanopoulos <konstantinostef@yahoo.gr>
2023-12-14 11:12:47 +0200

    insert generic x-layout in order to make satatables work

--- resources/views/public/school_areas.blade.php
+++ resources/views/public/school_areas.blade.php
@@ -1,35 +1,5 @@
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    @stack('title')
-    <link rel="stylesheet" href="{{url('/bootstrap/css/bootstrap.css')}}" rel="stylesheet"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.11/clipboard.min.js"></script>
-    <link rel="icon" href="{!! asset(url('/favicon/favicon.ico')) !!}"/>
-    <link rel="apple-touch-icon" sizes="180x180" href="{{url('/favicon/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{url("/favicon/favicon-32x32.png")}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{url("/favicon/favicon-16x16.png")}}">
-    <link rel="manifest" href="{{url('/favicon/site.webmanifest')}}">
-    <link rel="mask-icon" href="{{url('/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
-    <link href="{{url('/fontawesome-free-6.4.2-web/css/fontawesome.css')}}" rel="stylesheet">
-    <link href="{{url('/fontawesome-free-6.4.2-web/css/brands.css')}}" rel="stylesheet">
-    <link href="{{url('/fontawesome-free-6.4.2-web/css/solid.css')}}" rel="stylesheet">
-    <link href="{{url('/fontawesome-free-6.4.2-web/css/regular.css')}}" rel="stylesheet">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-    @stack('links')
-  </head>
-  <body>
-  
-  
-  <div class="mx-5"> <!-- Custom container --> 
-  @include('components/messages')    <!-- Show Notifications --> 
-
+<x-layout>
     @push('links')
         <link href="../DataTables-1.13.4/css/dataTables.bootstrap5.css" rel="stylesheet"/>
         <link href="../Responsive-2.4.1/css/responsive.bootstrap5.css" rel="stylesheet"/>
@@ -111,19 +81,4 @@
 
 </div> <!-- End of custom container --> 
 
-<!-- footer begins -->
-<footer class="border-top text-center small text-muted py-3">
-  <p class="m-0">Copyright &copy; {{Illuminate\Support\Carbon::now()->year}} <a href="{{url("/")}}" class="text-muted">e-forms</a>. Διεύθυνση Π.Ε. Αχαΐας - Τμήμα Πληροφορικής & Νέων Τεχνολογιών - Ηλεκτρονικές Υπηρεσίες.</p>
-</footer>
-<script src="{{url('/bootstrap/js/bootstrap.js')}}"></script>
-<script
-  src="https://code.jquery.com/jquery-3.6.4.min.js"
-  integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
-  crossorigin="anonymous">
-</script>
-@stack('scripts')
-</div> <!-- container closing -->
-  <div class="d-flex justify-content-center"><p class="h3" style="color:black"> {{env('APP_NAME')}}</p></div>
-  @stack('copy_script')
-</body>
-</html>
+</x-layout>

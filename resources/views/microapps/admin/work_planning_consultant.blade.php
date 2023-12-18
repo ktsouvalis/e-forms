d5a7b66102edfbd1388ca64e8bb07c7693aaf757
Konstantinos Stefanopoulos <konstantinostef@yahoo.gr>
2023-12-18 12:08:26 +0200

    implement admin's blade for consultants work planning

--- resources/views/microapps/admin/work_planning_consultant.blade.php
+++ resources/views/microapps/admin/work_planning_consultant.blade.php
@@ -18,6 +18,9 @@
         $selected_day7 = $selected_day->add(7,'day');
         $day_array = [$selected_day,$selected_day7];
     @endphp
+    @php
+        $user = App\Model\Consultant::get()->first;
+    @endphp
     @foreach($day_array as $today)
     <hr>
     @php

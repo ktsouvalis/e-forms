152b7a847f9cc78621fc0eaff5a61df78e0df6e9
Konstantinos Tsouvalis <ktsouvalis@sch.gr>
2024-02-26 11:32:29 +0200

    fix close button

--- resources/views/filecollects/edit.blade.php
+++ resources/views/filecollects/edit.blade.php
@@ -20,6 +20,9 @@
                     var stakeholderId = $(this).data('stakeholder-id');
                     $('#stakeholderId').val(stakeholderId);
                     $('#messageModal').modal('show');
+                    $('#messageModal').on('shown.bs.modal', function() {
+                        $('#message').focus();
+                    });
                 });
             });
         </script>
@@ -217,13 +220,18 @@
         <div class="vstack gap-2 py-3">
             
             @if($filecollect->stakeholders->count())
+            <script>
+                $(document).ready(function() {
+                    
+                });
+            </script>
             <div class="modal fade" id="messageModal" tabindex="-1" role="dialog" aria-labelledby="messageModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
+                <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="messageModalLabel">Αποστολή μηνύματος</h5>
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                {{-- <span aria-hidden="true">&times;</span> --}}
                             </button>
                         </div>
                         <form action="{{url("/filecollects/send_personal_message")}}" method="post">
@@ -243,6 +251,14 @@
                     </div>
                 </div>
             </div>
+
+            <script>
+                $(document).ready(function() {
+                    $('#messageModal').on('shown.bs.modal', function() {
+                        $('#message').focus();
+                    });
+                });
+            </script>
             <div class="table-responsive">
                 <table  id="dataTable" class="align-middle table table-sm table-striped table-hover">
                 <thead>

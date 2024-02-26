a036c4f84ceb87a8ecc489610e00a08a12e124c5
Konstantinos Tsouvalis <ktsouvalis@sch.gr>
2024-02-26 11:02:32 +0200

    fix close button in modal

--- resources/views/filecollects/edit.blade.php
+++ resources/views/filecollects/edit.blade.php
@@ -222,7 +222,7 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="messageModalLabel">Αποστολή μηνύματος</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
@@ -236,6 +236,7 @@
                                 </div>
                             </div>
                             <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                 <button type="submit" class="btn btn-primary bi bi-send"> Αποστολή</button>
                             </div>
                         </form>

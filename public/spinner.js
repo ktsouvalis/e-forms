24cc5223abb954be0827f70e75708ee0e3747551
Konstantinos Tsouvalis <ktsouvalis@sch.gr>
2024-05-09 11:51:32 +0300

    remove spinner for further test

--- public/spinner.js
+++ public/spinner.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
-    // $('#loadingSpinner').hide();
-    // Show spinner on link click
-    $('a').on('click', function () {
-        $('#loadingOverlay').show();
-        $('#loadingSpinner').css('display', 'flex'); // or 'block'
-    });
+// $(document).ready(function () {
+//     // $('#loadingSpinner').hide();
+//     // Show spinner on link click
+//     $('a').on('click', function () {
+//         $('#loadingOverlay').show();
+//         $('#loadingSpinner').css('display', 'flex'); // or 'block'
+//     });
 
-    // Show spinner on form submit
-    $('form[method="post"]').on('submit', function () {
-        $('#loadingOverlay').show();
-        $('#loadingSpinner').css('display', 'flex'); // or 'block'
-    });
+//     // Show spinner on form submit
+//     $('form[method="post"]').on('submit', function () {
+//         $('#loadingOverlay').show();
+//         $('#loadingSpinner').css('display', 'flex'); // or 'block'
+//     });
 
-    // Hide spinner when page is fully loaded
-    $(window).on('load', function () {
-        $('#loadingOverlay').hide();
-        $('#loadingSpinner').hide();
-    });
-});
+//     // Hide spinner when page is fully loaded
+//     $(window).on('load', function () {
+//         $('#loadingOverlay').hide();
+//         $('#loadingSpinner').hide();
+//     });
+// });

2408e370e5117ef0b029dbf6669f4a4bf0d72f87
Konstantinos Stefanopoulos <konstantinostef@yahoo.gr>
2024-06-10 22:01:07 +0300

    fix bug with multiple secondments creation with browser back and forward

--- app/Http/Controllers/SecondmentController.php
+++ app/Http/Controllers/SecondmentController.php
@@ -167,11 +167,13 @@ class SecondmentController extends Controller
         {
             try{
                 $teacher = Auth::guard('teacher')->user();
-                $secondment = new Secondment();
-                $secondment->teacher_id = $teacher->id;
-                $secondment->statement_of_declaration = true;
-                $secondment->application_for_reposition = (isset($request->application_for_reposition)? true : false);
-                $secondment->save();
+                $secondment = Secondment::updateOrCreate(
+                    ['teacher_id' => $teacher->id,
+                    'revoked' => 0],
+                    [$secondment->statement_of_declaration => true,
+                    $secondment->application_for_reposition = (isset($request->application_for_reposition)? true : false),
+                    ]
+                );
                 return redirect(route('secondments.edit', ['secondment' => $secondment->id]))->with('success',"Επιτυχής αποθήκευση αίτησης. Μπορείτε να προχωρήσετε σε δήλωση μοριοδοτούμενων κριτηρίων.");
             } catch(\Exception $e) {
                 //dd($e);

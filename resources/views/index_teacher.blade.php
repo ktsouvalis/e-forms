3b1ed94760943394c74b899ad8dbcfd8fc164502
Konstantinos Stefanopoulos <konstantinostef@yahoo.gr>
2025-06-23 17:49:51 +0300

    remove message to teachers blade

--- resources/views/index_teacher.blade.php
+++ resources/views/index_teacher.blade.php
@@ -151,11 +151,7 @@
                                 </div>
                             </div>
                         @endforeach
-                        <div>
-                            <p class="text-center" style="color: #d9534f; font-weight: bold;">
-                                Τα έντυπα αναπληρωτών θα είναι διαθέσιμα από τη Δευτέρα 23 Ιουνίου 2025 και ώρα 17:00 λόγω τεχνικού προβλήματος.
-                            </p>
-                        </div>
+                        
                         <hr>
                         <div class="col-md-4 py-2" style="max-width:15rem">
                             <div class="card py-5" style="background-color:Gainsboro; text-decoration:none; text-align:center;">

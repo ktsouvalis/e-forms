d2dea8b5102cdfbe8f8f7444105790fa038d8d7f
Kostas Stefanopoulos <konstantinostef@yahoo.gr>
2025-11-19 11:10:37 +0200

    create endpoint for fetching teachers data in modal of teachers blade

--- resources/views/teachers-gen.blade.php
+++ resources/views/teachers-gen.blade.php
@@ -54,14 +54,14 @@
                     var teacherId = $(this).data('teacher-id');
                     // var teacher = teachers.find(teacher => teacher.id == teacherId);
                     // var workExperience = teachers.find(teacher => teacher.id == teacherId).work_experience;
-                    console.log('Fetching data for teacher ID:', teacherId);
+                    //console.log('Fetching data for teacher ID:', teacherId);
                     $.ajax({
-                        url: '/api/teachers/' + teacherId, // Adjust this URL to match your API endpoint
+                        url: '{{ url("api/teachers") }}/' + teacherId,
                         method: 'GET',
                         dataType: 'json',
                         success: function(teacher) {
                             // Access teacher data
-                            console.log('ok', teacher);
+                            //console.log('ok', teacher);
                             var workExperience = teacher.work_experience;
                             
                             $('#infoModal .modal-body p:eq(0)').text('Επώνυμο: ' + teacher.surname);

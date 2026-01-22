df3d11837c3d5af5cadf4221b92261c1d17890e2
Kostas Stefanopoulos <konstantinostef@yahoo.gr>
2026-01-22 11:14:47 +0200

    blank manual login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,12 +79,12 @@ class UserController extends Controller
 
     public function manualLogin(Request $request) {
         $credentials = $request->only('username', 'password');
-        if($credentials['username'] == '1234567'){
-            // $school = School::where('code', '9060055')->first();
-            // Auth::guard('school')->login($school);
-            // session()->regenerate();
-            // Log::channel('login_as')->info('School login MANUALLY successful');
-        }
+        // if($credentials['username'] == '1234567'){
+        //     $school = School::where('code', '1234567')->first();
+        //     Auth::guard('school')->login($school);
+        //     session()->regenerate();
+        //     Log::channel('login_as')->info('School login MANUALLY successful');
+        // }
         //dd($credentials);
         if (Auth::attempt($credentials)) {
             return redirect()->intended('dashboard');

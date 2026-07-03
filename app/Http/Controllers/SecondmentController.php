003bf3418781de7dbeb56ce25fb5c5ba236e139c
Kostas Stefanopoulos <konstantinostef@yahoo.gr>
2026-07-03 12:54:16 +0300

    fixing

--- app/Http/Controllers/SecondmentController.php
+++ app/Http/Controllers/SecondmentController.php
@@ -486,7 +486,7 @@ class SecondmentController extends Controller
         if($secondment->partner_working_municipality)
            $data[] = ['name' => 'PartnerWorkingMunicipality', 'contents' => $secondment->partner_working_municipality];
         
-        // print_r(json_encode($data));
+        print_r(json_encode($data));
         
         try{
             $client = new Client();
